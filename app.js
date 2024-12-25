bbe17c43456271ef8a227b9085a69774ee7946f9
BalaK <balak@sadurasoftware.com>
2024-12-25 16:52:43 +0530

    port 4000 change

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ app.get('/', (req, res) => {
   });
 app.use('/api/users', userRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
